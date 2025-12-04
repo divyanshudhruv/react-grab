39931dda79629351099e1381ce906b7023c86f90
Aiden Bai <aiden.bai05@gmail.com>
2025-12-03 21:45:13 -0800

    Optimize element detection by caching overlay state and managing pointer events for improved performance

--- packages/react-grab/src/utils/get-element-at-position.ts
+++ packages/react-grab/src/utils/get-element-at-position.ts
@@ -1,10 +1,26 @@
 import { isValidGrabbableElement } from "./is-valid-grabbable-element.js";
+import { ATTRIBUTE_NAME } from "./mount-root.js";
+
+let cachedOverlay: HTMLElement | null = null;
 
 export const getElementAtPosition = (
   clientX: number,
   clientY: number,
 ): Element | null => {
+  if (!cachedOverlay || !cachedOverlay.isConnected) {
+    cachedOverlay = document.querySelector(`[${ATTRIBUTE_NAME}]`);
+  }
+
+  const savedPointerEvents = cachedOverlay?.style.pointerEvents;
+  if (cachedOverlay) {
+    cachedOverlay.style.pointerEvents = "none";
+  }
+
   let element = document.elementFromPoint(clientX, clientY);
+
+  if (cachedOverlay) {
+    cachedOverlay.style.pointerEvents = savedPointerEvents ?? "";
+  }
 
   while (element) {
     if (isValidGrabbableElement(element)) {
