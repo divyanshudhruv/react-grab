c60e65f79fbbfb10b0f58d0019c8f7ef598bf8c0
Aiden Bai <aiden.bai05@gmail.com>
2025-12-28 12:51:53 -0800

    fix footer

--- packages/website/components/demo-footer.tsx
+++ packages/website/components/demo-footer.tsx
@@ -15,16 +15,6 @@ export const DemoFooter = () => {
   return (
     <div className="pt-4 text-sm text-white/50">
       <p>
-        created by{" "}
-        <a
-          href="https://million.dev"
-          target="_blank"
-          rel="noreferrer"
-          className="underline underline-offset-4 hover:text-white/80"
-        >
-          million.dev
-        </a>{" "}
-        &middot;{" "}
         <button
           type="button"
           onClick={handleRestartClick}
